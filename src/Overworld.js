4aca247d239947aed0435467cbecec688195935f
Patrick Hu <phhu@ucsc.edu>
2023-11-17 18:54:10 -0800

    All files complete

---
+++ src/Overworld.js
@@ -0,0 +1,83 @@
+class Overworld extends Phaser.Scene {
+    constructor() {
+        super('overworldScene')
+    }
+
+    preload() {
+        this.load.path = './assets/'
+        this.load.spritesheet('slime', 'slime.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        })
+
+        this.load.image('tilesetImage', 'tileset.png')
+        this.load.tilemapTiledJSON('tilemapJSON', 'overworld.json')
+    }
+
+    create() {
+        // velocity constant
+        this.VEL = 100
+
+        // tilemap info
+        const map = this.add.tilemap('tilemapJSON')
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage')
+        const bg = map.createLayer('background', tileset, 0, 0)
+        const terrainLayer = map.createLayer('terrain', tileset, 0, 0)
+        const slimeSpawn = map.findObject('spawns', obj => obj.name === 'slimeSpawn')
+
+        // add slime
+        this.slime = this.physics.add.sprite(slimeSpawn.x, slimeSpawn.y, 'slime', 0)
+        this.slime.body.setCollideWorldBounds(true)
+
+        const treeLayer = map.createLayer('trees', tileset, 0, 0)
+
+        // slime animation
+        this.anims.create({
+            key: 'jiggle',
+            frameRate : 8,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime', {
+                start: 0,
+                end: 1
+            })
+        })
+
+        this.slime.play('jiggle')
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+        this.cameras.main.startFollow(this.slime, true, 0.25, 0.25)
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+
+        terrainLayer.setCollisionByProperty({
+            collides: true
+        })
+        this.physics.add.collider(this.slime, terrainLayer)
+
+        treeLayer.setCollisionByProperty({
+            collides: true
+        })
+        this.physics.add.collider(this.slime, treeLayer)
+
+        // input
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // slime movement
+        this.direction = new Phaser.Math.Vector2(0)
+        if(this.cursors.left.isDown) {
+            this.direction.x = -1
+        } else if(this.cursors.right.isDown) {
+            this.direction.x = 1
+        }
+
+        if(this.cursors.up.isDown) {
+            this.direction.y = -1
+        } else if(this.cursors.down.isDown) {
+            this.direction.y = 1
+        }
+
+        this.direction.normalize()
+        this.slime.setVelocity(this.VEL * this.direction.x, this.VEL * this.direction.y)
+    }
+}
